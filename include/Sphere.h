c9d8fb63e135227c4a1db882f9ab4d9d225a0396
Máté Timkó <timko.mate@gmail.com>
2019-03-05 19:37:00 +0100

    first commit

---
+++ include/Sphere.h
@@ -0,0 +1,19 @@
+//
+// Created by timko on 2019.02.12..
+//
+
+#ifndef GRAVITYSPHERE_SPHERE_H
+#define GRAVITYSPHERE_SPHERE_H
+
+#include<iostream>
+class Sphere{
+    private:
+        coordX;
+        coordZ;
+        deltaRho;
+        radius;
+
+};
+
+
+#endif //GRAVITYSPHERE_SPHERE_H
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int args, char ** argv){
+    std::cout << "Hello world!!" << std::endl;
+    return 0;
+}
